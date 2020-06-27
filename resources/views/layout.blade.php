405ff91bf78cd2e3bfbc75b1e3749f170a8d8452
Dagmar Cirino <dagmar.mv@gmail.com>
2020-06-27 05:37:05 -0300

    Initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Controle de Séries</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
+        </div>
+        @endif
+
+        <div class="jumbotron">
+            <h1>@yield('cabecalho')</h1>
+        </div>
+
+        @yield('conteudo')
+    </div>
+</body>
+</html>
